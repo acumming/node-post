cc9f69464a3fb0a628c19ae545adcab4ff6beed1
Alec Cumming <alec.cumming@gmail.com>
2012-01-01 16:21:28 -0500

    cleanup
    Signed-off-by: Alec Cumming <alec.cumming@gmail.com>

--- drivers/mongo.js
+++ drivers/mongo.js
@@ -1,4 +1,3 @@
 var db_config = require("../config/db").config;
 var mongodb = require("mongoskin");
-
 exports.db = mongodb.db(db_config.db_host + ":" + db_config.db_port + "/" + db_config.db_name);
